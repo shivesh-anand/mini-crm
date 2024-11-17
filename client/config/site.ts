751d96560d97c851a5484d91d24a26eea4fd9575
shivesh-anand <btech10218.21@bitmesra.ac.in>
2024-11-17 23:49:44 +0530

    site config changes

--- client/config/site.ts
+++ client/config/site.ts
@@ -15,15 +15,15 @@ export const siteConfig = {
   ],
   navMenuItems: [
     {
-      label: "Home",
-      href: "/",
+      label: "Audience",
+      href: "/audience",
+    },
+    {
+      label: "Campaigns",
+      href: "/campaigns",
     },
   ],
   links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    github: "https://github.com/shivesh-anand/mini-crm-client",
   },
 };
